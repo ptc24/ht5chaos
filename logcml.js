62ca55022232574d43bc34c05aab70034cb68a00
Peter Corbett <peter.corbett@cantab.net>
2014-03-17 19:06:47 +0000

    Added LogCML

--- logcml.js
+++ logcml.js
@@ -1,4 +1,4 @@
-// Kuramoto model
+// Logistic Map CML
 // copyright (c) 2014 Peter Corbett - see LICENSE
 
 var canvas = document.getElementById('canvas');
@@ -10,43 +10,25 @@ canvas.height = 400;
 var img = 0;
 var record = 0;
 
-var perframe = 1;
-var deltat = 0.001;
-
 var running = 0;
 var simtime = 0;
 var frames = 0;
 
 var citer = 0;
 
-var thetas = [];
-var nthetas = [];
-var omegas = [];
+var xs = [];
+var nxs = [];
+var rs = [];
 var ks = [];
 var noises = [];
 
-var tstr = "";
+var xstr = "";
 var nstr = "";
-var ostr = "";
+var rstr = "";
 var kstr = "";
 
 var k = 1.0;
-var omega = 1.0;
-
-// Degrees radians
-var d2r = Math.PI / 180;
-var r2d = 1 / d2r;
-
-var rlookup = [];
-var blookup = [];
-var glookup = [];
-
-for(var i=0;i<=3600;i++) {
-	var rgb = tinycolor({h:i/10.0,s:1,v:1}).toRgb();
-	rlookup.push(rgb.r);
-	glookup.push(rgb.g);
-	blookup.push(rgb.b);
-}
+var r = 1.0;
 
 // Box-Muller Transform Gaussian
 function rnd_gauss() {
@@ -61,11 +43,9 @@ function rnd_cauchy() {
 function readform() {
 	canvas.width = parseFloat(document.getElementById("gwidth").value);
 	canvas.height = parseFloat(document.getElementById("gheight").value);
-	perframe = parseInt(document.getElementById("perframe").value);
-	deltat = parseFloat(document.getElementById("deltat").value);
 
-	tstr = document.getElementById("tstr").value
-	ostr = document.getElementById("ostr").value
+	xstr = document.getElementById("xstr").value
+	rstr = document.getElementById("rstr").value
 	kstr = document.getElementById("kstr").value
 	nstr = document.getElementById("nstr").value
 	
@@ -82,15 +62,14 @@ function render() {
 	var data = img.data;
 
 	for(var i=0;i<canvas.width;i++) {
+		console.log(i);
 		for(var j=0;j<canvas.height;j++) {
 			var idx = ((canvas.height-j-1) * canvas.width + i) * 4;
 			
-			var nt = thetas[i][j] * r2d;
-			while(nt < 0) nt += 360;
-			while(nt > 360) nt -= 360;
-			var r = rlookup[Math.round(10*nt)];
-			var g = glookup[Math.round(10*nt)];
-			var b = blookup[Math.round(10*nt)];
+			var xx = xs[i][j] * 255;
+			var r = xx;
+			var g = xx;
+			var b = xx;
 			
 			data[idx] = r;
 			data[idx+1] = g;
@@ -114,29 +93,28 @@ function reset() {
 	running = 0;
 	crunning = 0;
 	readform();
-	simtime = 0;
 	frames = 0;
 	
-	thetas = [];
-	nthetas = [];
-	omegas = [];
+	xs = [];
+	nxs = [];
+	rs = [];
 	ks = [];
 	noises = [];
 	
 	for(var i=0;i<canvas.width;i++) {
-		thetas.push([]);
-		nthetas.push([]);
-		omegas.push([]);
+		xs.push([]);
+		nxs.push([]);
+		rs.push([]);
 		ks.push([]);
 		noises.push([]);
 		for(var j=0;j<canvas.height;j++) {
 			var x = i*1.0/canvas.width;
 			var y = j*1.0/canvas.height;
-			nthetas[i].push(0);
+			nxs[i].push(0);
 			noises[i].push(eval(nstr));
 			ks[i].push(eval(kstr));
-			thetas[i].push(eval(tstr));
-			omegas[i].push(eval(ostr));
+			xs[i].push(eval(xstr));
+			rs[i].push(eval(rstr));
 		}
 	}
 	render();
@@ -144,60 +122,38 @@ function reset() {
 
 function iterate() {
 	var t = Date.now();
-
-	for(var cyc=0;cyc<perframe;cyc++) {
-		simtime += deltat;
-		for(var i=0;i<canvas.width;i++) {
-			for(var j=0;j<canvas.height;j++) {
-				var pdiff = 0;
-				var n = 0;
-				var theta = thetas[i][j];
-				if(true) {
-					if(i>0) {
-						pdiff += Math.sin(thetas[i-1][j] - theta);
-						n++;
-					}
-					if(j>0) {
-						pdiff += Math.sin(thetas[i][j-1] - theta);
-						n++;
-					}
-					if(i<canvas.width-1) {
-						pdiff += Math.sin(thetas[i+1][j] - theta);
-						n++;
-					}
-					if(j<canvas.height-1) {
-						pdiff += Math.sin(thetas[i][j+1] - theta);
-						n++;
-					}
-				} else {
-					// 8-adjacent code, just slows things down to no good effect,
-					// but I though I'd keep it there in case I or anyone else wants
-					// to experiment.
-					for(var ii=-1;ii<2;ii++) {
-						if(i+ii<0 || i+ii >= canvas.width) continue;
-						for(var jj=-1;jj<2;jj++) {
-							if(j+jj<0 || j+jj >= canvas.height) continue;
-							if(ii==0 && jj==0) continue;
-							pdiff += Math.sin(thetas[i+ii][j+jj] - theta);
-							n++;
-						}
-					}
-				}
-				var noise = Math.random()-0.5;
-				noise *= noises[i][j];
-				nthetas[i][j] = thetas[i][j] + deltat * (omegas[i][j] + (ks[i][j] * pdiff / n) + noise);
-			}
+	for(var i=0;i<canvas.width;i++) {
+		for(var j=0;j<canvas.height;j++) {
+			nxs[i][j] = rs[i][j]*xs[i][j]*(1.0-xs[i][j]);
 		}
-		
-		for(var i=0;i<canvas.width;i++) {
-			for(var j=0;j<canvas.height;j++) {
-				thetas[i][j] = nthetas[i][j];
+	}
+	for(var i=0;i<canvas.width;i++) {
+		for(var j=0;j<canvas.height;j++) {
+			var xt = nxs[i][j];
+			var n = 1;
+			if(i>0) {
+				xt += nxs[i-1][j];
+				n++;
 			}
+			if(j>0) {
+				xt += nxs[i][j-1];
+				n++;
+			}
+			if(i<canvas.width-1) {
+				xt += nxs[i+1][j];
+				n++;
+			}
+			if(j<canvas.height-1) {
+				xt += nxs[i][j+1];
+				n++;
+			}
+			xs[i][j] = (ks[i][j] * (xt / n)) + ((1-ks[i][j]) * nxs[i][j]);
+			if(noises[i][j] > 0) xs[i][j] = (Math.random() * noises[i][j]) + ((1-noises[i][j])*xs[i][j]);
 		}
 	}
 	frames++;
 	var e = document.getElementById("status");
-	e.innerHTML = "Time per frame: " + (Date.now() - t) + " Frames so far " + frames + " t " + simtime.toFixed(Math.ceil(-Math.log(deltat)/Math.LN10));
+	e.innerHTML = "Time per frame: " + (Date.now() - t) + " Frames so far " + frames;
 	render();
 	if(running == 1) setTimeout(iterate, 10);
 }
@@ -211,7 +167,7 @@ function mousedown(event) {
 	var cx = (event.pageX-canvas.offsetLeft);
 	var cy = (event.pageY-canvas.offsetTop);
 	var e = document.getElementById("status");
-	e.innerHTML = "";
+	e.innerHTML = "x = " + (cx / canvas.width) + " y= " + (1.0 - (cy / canvas.height));
 }
 
 function matmult(m1, m2) {
@@ -233,6 +189,6 @@ function autoiterate() {
 	iterate();
 }
 
-//canvas.addEventListener("mousedown", mousedown, false);
+canvas.addEventListener("mousedown", mousedown, false);
 
 reset();
